cc9d6f8dd5974707703b84f9242c671fcb3953db
lsywind3 <lsywind3@gmail.com>
2016-06-01 00:07:34 +0900

    다 이루었노라.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var latitude = 0;
+var longitude = 0;
+var signal = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.all('/signal', function(req, res, next) {
-  var latitude = req.body.latitude || req.query.latitude;
-  var longitude = req.body.longitude || req.query.longitude;
-  var signal = req.body.signal || req.query.signal;
+  latitude = req.body.latitude || req.query.latitude;
+  longitude = req.body.longitude || req.query.longitude;
+  signal = req.body.signal || req.query.signal;
 
   res.json({
     result: 1,
+    latitude: latitude,
+    longitude: longitude,
+    signal: signal
+  });
+});
+
+router.all('/send', function(req, res, next) {
+  res.json({
+    msg: latitude,
     latitude: latitude,
     longitude: longitude,
     signal: signal
