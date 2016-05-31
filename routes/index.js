fc46889f79cdfa785915dd9b3132cc47b22e5d88
Sang Yun Lee <lsywind3@gmail.com>
2016-05-31 22:24:18 +0900

    port 변경 및 signal 라우트 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.all('/signal', function(req, res, next) {
+  var latitude = req.body.latitude || req.query.latitude;
+  var longitude = req.body.longitude || req.query.longitude;
+  var signal = req.body.signal || req.query.signal;
+
+  res.json({
+    result: 1,
+    latitude: latitude,
+    longitude: longitude,
+    signal: signal
+  });
+});
+
 module.exports = router;
