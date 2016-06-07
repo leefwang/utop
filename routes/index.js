e1931f2c2610c1045964ac9234ce1a0ae4d42783
Sang Yun Lee <lsywind3@gmail.com>
2016-06-07 17:11:55 +0900

    signin 추가

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.all('/signin', function(req, res, next) {
+  var userInfo = [{
+    id: "admin",
+    password: "1234"
+  }, {
+    id: "client",
+    password: "1234"
+  }];
+
+  for (i = 0; i < userInfo.length; i++) {
+    if (userInfo[i].id === req.body.id && userInfo[i].password === req.body.password) {
+      return res.json({
+        result: 1
+      });
+    }
+  }
+
+  res.json({
+    result: 0,
+    msg: "Invalid ID or password"
+  });
+});
+
 router.all('/signal', function(req, res, next) {
   latitude = req.body.latitude || req.query.latitude;
   longitude = req.body.longitude || req.query.longitude;
